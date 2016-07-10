2ce9f3743debda7599d2a12d3a272ff1510408bc
Jerry Lan <1577628623@qq.com>
2016-07-10 21:57:11 +0800

    Update BmobConfig.class.php

--- lib/BmobConfig.class.php
+++ lib/BmobConfig.class.php
@@ -8,8 +8,8 @@
  */
 class BmobConfig
 {
-     const APPID = '16d8719fe7812a0ae2e05ef5a097706e';  //后台"应用密钥"中的Application ID
-     const RESTKEY = 'c028a09353ac0d5af5c37fe44c4dae10';  //后台"应用密钥"中的REST API Key
+     const APPID = '';  //后台"应用密钥"中的Application ID
+     const RESTKEY = '';  //后台"应用密钥"中的REST API Key
      const BMOBURL = 'https://api.bmob.cn/1/';  //请勿修改
      const BMOBURLTWO = 'https://api.bmob.cn/2/';  //请勿修改
 
